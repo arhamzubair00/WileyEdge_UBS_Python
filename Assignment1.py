f6255f6cb41a7bcc1712e8a88d69715a7ed2e148
arham zubair arhamzubair00@gmail.com <arhamzubair00@gmail.com>
2022-09-21 21:31:47 +0100

    Program complete - asks user for coffee size, type and flavouring. Calculates total throughout, and provides summary at the end.

--- Assignment1.py
+++ Assignment1.py
@@ -2,9 +2,52 @@
 # Ask the user what size cup they want, choosing between small, medium, and large.
 # Ask the user what kind of coffee they want, choosing between brewed, espresso, and cold brew.
 # Ask the user what flavoring they want, if any. Choices include hazelnut, vanilla, and caramel.
+# Store types of coffee size, coffee kind, and flavour in dictionaries
+# Add cost to one variable, throughout the order
+
+import sys
+
+
+total_cost = 0
+
+sizes = {
+    "small": 2,
+    "medium": 3,
+    "large": 4
+    }
+
+types = {
+    "brewed": 0,
+    "espresso": 0.5,
+    "cold brew": 1
+    }
 
 coffee_size = input("Would you like a small, medium, or large? ")
-print(f'You selected the {coffee_size} size')
+if coffee_size == "small" or coffee_size == "medium" or coffee_size == "large":
+    print(f'You selected the {coffee_size} size.')
+    total_cost = sizes[f"{coffee_size}"]
+else:
+    sys.exit("You provided an incorrect response, let's start again. ")
+
+
 
 coffee_kind = input("Would you like brewed, espresso, or a cold brew? ")
-print(f"You selected {coffee_kind} coffee. ")
+if coffee_kind == "brewed" or coffee_kind == "espresso" or coffee_kind == "cold brew":
+    print(f"You selected {coffee_kind} coffee. ")
+    total_cost = total_cost + types[f"{coffee_kind}"]
+else:
+    sys.exit("You provided an incorrect response, let's start again. ")
+
+
+flavour = input("Would you like to add a flavouring? Choices include hazelnut, vanilla, and caramel. ")
+if flavour == "hazelnut" or flavour == "vanilla" or flavour == "caramel":
+    print(f"You selected the {flavour} syrup. ")
+    total_cost = total_cost + 0.5
+elif flavour == "no":
+    print("You did not select a syrup.")
+else:
+    sys.exit("You provided an incorrect response, let's start again. ")
+
+
+print(f"To summarise, you asked for a {coffee_size} cup of {coffee_kind} coffee, with {flavour} syrup. ")
+print(f"Your total comes to ${total_cost}. ")
