4a799bc3462988c5845d6f398328a48a2a8ca153
arham zubair arhamzubair00@gmail.com <arhamzubair00@gmail.com>
2022-09-21 20:36:30 +0100

    Pushing assignment 1 (first commit)

---
+++ Assignment1.py
@@ -0,0 +1,10 @@
+# Pseudocode
+# Ask the user what size cup they want, choosing between small, medium, and large.
+# Ask the user what kind of coffee they want, choosing between brewed, espresso, and cold brew.
+# Ask the user what flavoring they want, if any. Choices include hazelnut, vanilla, and caramel.
+
+coffee_size = input("Would you like a small, medium, or large? ")
+print(f'You selected the {coffee_size} size')
+
+coffee_kind = input("Would you like brewed, espresso, or a cold brew? ")
+print(f"You selected {coffee_kind} coffee. ")
