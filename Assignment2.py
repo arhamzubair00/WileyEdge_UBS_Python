de6e90b7862a597a7c841458d879636d5026f274
arham zubair arhamzubair00@gmail.com <arhamzubair00@gmail.com>
2022-09-25 17:46:51 +0100

    loop created to check if dictionary contains the words from the list and updating their values accordingly.

--- Assignment2.py
+++ Assignment2.py
@@ -9,6 +9,10 @@
 # 6. Remove the punctuation from the lowercase string.
 # 7. Perform a count analysis on the text without punctuation characters.
 
+# Importing the string module, which includes the punctuations too
+# Creating a punctuation list only
+import string
+punctuation = string.punctuation
 
 default_para = """Imagine a vast sheet of paper on which straight Lines, Triangles, Squares, Pentagons, Hexagons, and other figures, instead of remaining fixed in their places, move freely about, on or in the surface, but without the power of rising above or sinking below it, very much like shadows - only hard and with luminous edges - and you will then have a pretty correct notion of my country and countrymen. Alas, a few years ago, I should have said "my universe": but now my mind has been opened to higher views of things."""
 
@@ -31,15 +35,14 @@ words_set_length = len(words_set)
 print(f"After the conversion from list to set, the length of the set is: {words_set_length}")
 
 # creating an empty dictionary
-dictionary = dict()
+word_freq = dict()
 
-# for word in words_list:
-    # if word ==
+# for each word in the list, check if the word is already in the dictionary
+for word in words_list:
+    if word in word_freq:
+        word_freq[word] += 1
+    else:
+        word_freq[word] = 1
 
 
-
-
-print(words_set)
-
-print(words_list)
-# do not write any code past here
+print(word_freq)
